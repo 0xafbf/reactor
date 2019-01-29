8bfade3dfa24f94b77f08250ba71426e2a07cbb7
Andres Botero <0xafbf@gmail.com>
2019-01-29 12:18:08 -0500

    improved setup script

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,19 @@
 
+push-location $PSScriptRoot
+
 # get submodules
 echo "updating submodules..."
 # this takes a long time, maybe have some cache in place?
-& git submodule update --init --recursive
+& git submodule update --init
+
+push-location "deps/slang"
+& git submodule update --init external/glslang
+pop-location
+
+
 
 echo "running premake..."
 
 & ./external/premake/premake5 vs2017 --verbose
+
+pop-location
